ca69dfb5f7fc3e8f35c4c3a478ed4ce00cf9edce
Alexey Malev <amalev@yoomoney.ru>
2025-02-11 13:51:13 +0300

    Изменена версия набора изменений

--- Common/distro.inc.php
+++ Common/distro.inc.php
@@ -1,6 +1,6 @@
 <?php
 define('ProgramRelease', 'STABLE');
-define('ProgramBuild', 'rev 26 [improvements v1.1]');
+define('ProgramBuild', 'rev 26 [improvements v2.0]');
 define('MinTimeOut', '120');
 //Add the Timezone Check if not setup by system
 if(strlen(ini_get('date.timezone')))
