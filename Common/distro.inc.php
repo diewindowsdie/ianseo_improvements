9d1de74dc5b9fde73050cc87fb440b235d0f2056
Alexey Malev <diewindowsdie@gmail.com>
2025-11-16 18:13:48 +0300

    ianseo 2025-09-03 rev47 v4.15

--- Common/distro.inc.php
+++ Common/distro.inc.php
@@ -1,6 +1,6 @@
 <?php
 define('ProgramRelease', 'STABLE');
-define('ProgramBuild', 'rev 47 [improvements v4.14.1]');
+define('ProgramBuild', 'rev 47 [improvements v4.15]');
 define('MinTimeOut', '120');
 //Add the Timezone Check if not setup by system
 if(strlen(ini_get('date.timezone')))
