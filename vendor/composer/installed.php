9195abc1cfa827126acab3bc5774dbceae1ee568
Alexey Malev <diewindowsdie@gmail.com>
2025-12-17 19:09:17 +0300

    Добавлена библиотека morphos как зависимость composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9e35b72155d0c5a49b170a3e1918978331da1776',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9e35b72155d0c5a49b170a3e1918978331da1776',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wapmorgan/morphos' => array(
+            'pretty_version' => '3.2.30',
+            'version' => '3.2.30.0',
+            'reference' => 'b7680f96101668ec910627239048e34aae64d6f8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wapmorgan/morphos',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
