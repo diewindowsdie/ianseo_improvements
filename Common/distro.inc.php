cdbf73ee24b487df419c4059c54c70cfae7b1aa0
Alexey Malev <diewindowsdie@gmail.com>
2026-01-24 22:33:00 +0300

    Релиз v5.3

--- Common/distro.inc.php
+++ Common/distro.inc.php
@@ -1,7 +1,7 @@
 <?php
 define('ProgramRelease', 'STABLE');
-define('ProgramBuild', 'rev 183 [improvements v5.2]');
-define("CurrentTag", "v5.2");
+define('ProgramBuild', 'rev 183 [improvements v5.3]');
+define("CurrentTag", "v5.3");
 define('MinTimeOut', '120');
 //Add the Timezone Check if not setup by system
 if(strlen(ini_get('date.timezone')))
