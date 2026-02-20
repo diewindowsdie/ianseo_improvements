5c72c15a3942445aeb2d83a00d117618203e2546
Alexey Malev <diewindowsdie@gmail.com>
2026-02-20 21:38:38 +0300

    Изменение версий обновлений ianseo для live-релизов

--- Common/distro.inc.php
+++ Common/distro.inc.php
@@ -1,9 +1,9 @@
 <?php
 define('ProgramRelease', 'STABLE');
-define('ProgramBuild', 'rev 254 [improvements v5.5]');
-define("CurrentTag", "v5.5");
+define('ProgramBuild', 'rev 254 [improvements LIVE v1.0]');
+define("CurrentTag", "live_v1.0");
 //pre-релизы собираются из ветки live - это lts ветка, предназначенная для проведения крупных соревнований, в нее попадают только критически важные обновления
-define("UsePreReleases", false);
+define("UsePreReleases", true);
 define('MinTimeOut', '120');
 //Add the Timezone Check if not setup by system
 if(strlen(ini_get('date.timezone')))
