cec8bf340cf9cfbca1bb32b3a6bfbe51bfb0a19e
Alexey Malev <diewindowsdie@gmail.com>
2026-02-22 22:25:20 +0300

    Подготовка релиза v5.6

--- Common/distro.inc.php
+++ Common/distro.inc.php
@@ -1,7 +1,7 @@
 <?php
 define('ProgramRelease', 'STABLE');
-define('ProgramBuild', 'rev 254 [improvements v5.5]');
-define("CurrentTag", "v5.5");
+define('ProgramBuild', 'rev 254 [improvements v5.6]');
+define("CurrentTag", "v5.6");
 //pre-релизы собираются из ветки live - это lts ветка, предназначенная для проведения крупных соревнований, в нее попадают только критически важные обновления
 define("UsePreReleases", false);
 define('MinTimeOut', '120');
