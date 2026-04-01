598e74c506c23a3555deea534289eb60356a9101
Alexey Malev <diewindowsdie@gmail.com>
2026-04-01 18:51:44 +0300

    В беларусских правилах на летних дистанциях применено ранжирование спортсменов в квалификации по Х, и затем по 10

--- Modules/Sets/BLR/Setup_1000_BLR.php
+++ Modules/Sets/BLR/Setup_1000_BLR.php
@@ -19,10 +19,10 @@ $tourDetMaxFinTeamScore	= '240';
 $tourDetCategory		= '2'; // 0: Other, 1: Outdoor, 2: Indoor, 4:Field, 8:3D
 $tourDetElabTeam		= '0'; // 0:Standard, 1:Field, 2:3DI
 $tourDetElimination		= '0'; // 0: No Eliminations, 1: Elimination Allowed
-$tourDetGolds			= '10+X';
-$tourDetXNine			= 'X';
-$tourDetGoldsChars		= 'KL';
-$tourDetXNineChars		= 'K';
+$tourDetGolds			= '10';
+$tourDetXNine			= '9';
+$tourDetGoldsChars		= 'L';
+$tourDetXNineChars		= 'J';
 $tourDetDouble			= '1';
 $DistanceInfoArray=array(array(6, 6), array(6, 6), array(6, 6), array(6, 6));
 
