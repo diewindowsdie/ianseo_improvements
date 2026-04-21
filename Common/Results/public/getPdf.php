49bcda14ebd45fa14f3202519bdc581aa68f3042
Alexey Malev <diewindowsdie@gmail.com>
2026-04-21 23:32:25 +0300

    При публикации результатов для ссылки на соревнование теперь везде используется его код вместо ID

--- Common/Results/public/getPdf.php
+++ Common/Results/public/getPdf.php
@@ -2,16 +2,6 @@
 require_once dirname(__FILE__, 4) . '/config.php';
 require_once dirname(__FILE__, 2) . "/lib.php";
 
-//проверим, что передан "код" отчета, который мы знаем и поддерживаем
-if (!in_array($_REQUEST["report"], array("qI", "qT", "bI", "bT", "rI", "rT", "qcI", "fcI", "fcT", "j", "d", "s", "r1", "r2", "r3", "sac", "sar", "sacd", "sqf", "fs", "m", "w", "p"))) {
-    exitNotFound();
-}
-
-//проверим, что передан код соревнования, и соревнование не скрыто от публикации
-if (!isset($_REQUEST["id"]) || getModuleParameter(RESULTS_PUBLICATION_MODULE_NAME, COMPETITION_HIDDEN_PARAM_NAME, "0", $_REQUEST["id"]) === "1") {
-    exitNotFound();
-}
-
 function getVisibleEventCodesConsideringTeams($competitionId, $isTeam): array
 {
     $events = array();
@@ -27,22 +17,36 @@ function getVisibleEventCodesConsideringTeams($competitionId, $isTeam): array
     return $events;
 }
 
+//проверим, что передан "код" отчета, который мы знаем и поддерживаем
+if (!in_array($_REQUEST["report"], array("qI", "qT", "bI", "bT", "rI", "rT", "qcI", "fcI", "fcT", "j", "d", "s", "r1", "r2", "r3", "sac", "sar", "sacd", "sqf", "fs", "m", "w", "p"))) {
+    exitNotFound();
+}
+
+//проверим, что передан код соревнования, и соревнование не скрыто от публикации
+if (!isset($_REQUEST["id"])) {
+    exitNotFound();
+}
+$competition = findCompetitionByCode($_REQUEST["id"]);
+if (!$competition || getModuleParameter(RESULTS_PUBLICATION_MODULE_NAME, COMPETITION_HIDDEN_PARAM_NAME, "0", $competition->TourId) === "1") {
+    exitNotFound();
+}
+
 //засунем в реквест общие параметры для всех отчетов
-$_REQUEST["TourId"] = $_REQUEST["id"];
+$_REQUEST["TourId"] = $competition->TourId;
 
 switch ($_REQUEST["report"]) {
     case 'qI':
-        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($_REQUEST["id"], "0");
+        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($competition->TourId, "0");
 
         require_once "../../../Qualification/PrintIndividual.php";
         break;
     case 'qT':
-        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($_REQUEST["id"], "1");
+        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($competition->TourId, "1");
 
         require_once "../../../Qualification/PrintTeam.php";
         break;
     case 'bI':
-        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($_REQUEST["id"], "0");
+        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($competition->TourId, "0");
 
         $_REQUEST["ShowTargetNo"] = "1";
         $_REQUEST["ShowSchedule"] = "1";
@@ -50,7 +54,7 @@ switch ($_REQUEST["report"]) {
         require_once "../../../Final/Individual/PrnBracket.php";
         break;
     case 'bT':
-        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($_REQUEST["id"], "1");
+        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($competition->TourId, "1");
 
         $_REQUEST["ShowTargetNo"] = "1";
         $_REQUEST["ShowSchedule"] = "1";
@@ -58,17 +62,17 @@ switch ($_REQUEST["report"]) {
         require_once "../../../Final/Team/PrnBracket.php";
         break;
     case 'rI':
-        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($_REQUEST["id"], "0");
+        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($competition->TourId, "0");
 
         require_once "../../../Final/Individual/PrnRanking.php";
         break;
     case 'rT':
-        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($_REQUEST["id"], "1");
+        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($competition->TourId, "1");
 
         require_once "../../../Final/Team/PrnRanking.php";
         break;
     case "qcI":
-        if (getModuleParameter(RESULTS_PUBLICATION_MODULE_NAME, PUBLISH_SCORECARDS_PARAM_NAME, "1", $_REQUEST["id"]) === "0") {
+        if (getModuleParameter(RESULTS_PUBLICATION_MODULE_NAME, PUBLISH_SCORECARDS_PARAM_NAME, "1", $competition->TourId) === "0") {
             exitNotFound();
         }
 
@@ -88,7 +92,7 @@ switch ($_REQUEST["report"]) {
                                         (QuD7Arrowstring != '')) as MaxDistances
                                 from Qualifications q
                                 left join Entries e on q.QuId = e.EnId
-                                where e.EnTournament = " . StrSafe_DB($_REQUEST["id"]) . " and q.QuSession = " . $_REQUEST["session"];
+                                where e.EnTournament = " . StrSafe_DB($competition->TourId) . " and q.QuSession = " . $_REQUEST["session"];
         $resultSet = safe_r_SQL($maxDistancesQuery);
         $_REQUEST["ScoreDist"] = range(1, safe_fetch($resultSet)->MaxDistances);
 
@@ -114,7 +118,7 @@ switch ($_REQUEST["report"]) {
         $index = substr($_REQUEST["report"], 1);
 
         $c = new stdClass();
-        $c->ToId = $_REQUEST["id"];
+        $c->ToId = $competition->TourId;
 
         if (getModuleParameter(PROTOCOL_MODULE, CHECKED_COUNTRY_PARAM_PREFIX . $index, "0", $c->ToId) === "0") {
             exitNotFound();
@@ -129,7 +133,7 @@ switch ($_REQUEST["report"]) {
         break;
     case "sac":
         $_REQUEST["SinglePage"] = "1";
-        $_REQUEST["TeamEvents"] = getVisibleEventCodesConsideringTeams($_REQUEST["id"], "0");
+        $_REQUEST["TeamEvents"] = getVisibleEventCodesConsideringTeams($competition->TourId, "0");
         unset($_REQUEST["Event"]);
 
         require_once "../../../Partecipants/PrnCategory.php";
@@ -147,28 +151,28 @@ switch ($_REQUEST["report"]) {
         require_once "../../../Partecipants/PrnStatEvents.php";
         break;
     case "fcI":
-        if (getModuleParameter(RESULTS_PUBLICATION_MODULE_NAME, PUBLISH_SCORECARDS_PARAM_NAME, "1", $_REQUEST["id"]) === "0") {
+        if (getModuleParameter(RESULTS_PUBLICATION_MODULE_NAME, PUBLISH_SCORECARDS_PARAM_NAME, "1", $competition->TourId) === "0") {
             exitNotFound();
         }
 
-        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($_REQUEST["id"], "0");
+        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($competition->TourId, "0");
         $_REQUEST["ScoreFilled"] = "1";
 
         require_once "../../../Final/Individual/PDFScoreMatch.php";
         break;
     case "fcT":
-        if (getModuleParameter(RESULTS_PUBLICATION_MODULE_NAME, PUBLISH_SCORECARDS_PARAM_NAME, "1", $_REQUEST["id"]) === "0") {
+        if (getModuleParameter(RESULTS_PUBLICATION_MODULE_NAME, PUBLISH_SCORECARDS_PARAM_NAME, "1", $competition->TourId) === "0") {
             exitNotFound();
         }
 
-        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($_REQUEST["id"], "1");
+        $_REQUEST["Event"] = getVisibleEventCodesConsideringTeams($competition->TourId, "1");
         $_REQUEST["ScoreFilled"] = "1";
 
         require_once "../../../Final/Team/PDFScoreMatch.php";
         break;
     case "fs":
         $_REQUEST["TeamComponents"] = "1";
-        $_REQUEST["ses"] = getModuleParameter(RESULTS_PUBLICATION_MODULE_NAME, SHOW_FINAL_SESSIONS_IN_SCHEDULE_PARAM_NAME, array(), $_REQUEST["id"]);
+        $_REQUEST["ses"] = getModuleParameter(RESULTS_PUBLICATION_MODULE_NAME, SHOW_FINAL_SESSIONS_IN_SCHEDULE_PARAM_NAME, array(), $competition->TourId);
 
         require_once "../../../Scheduler/OrisSchedule.php";
         break;
@@ -181,21 +185,21 @@ switch ($_REQUEST["report"]) {
     case "p":
         $_REQUEST["doPrint"] = "1";
 
-        $_REQUEST["Events"] = getVisibleEventCodesConsideringTeams($_REQUEST["id"], "0");
-        $_REQUEST["TeamEvents"] = getVisibleEventCodesConsideringTeams($_REQUEST["id"], "1");
+        $_REQUEST["Events"] = getVisibleEventCodesConsideringTeams($competition->TourId, "0");
+        $_REQUEST["TeamEvents"] = getVisibleEventCodesConsideringTeams($competition->TourId, "1");
 
         $c = new stdClass();
-        $c->ToId = $_REQUEST["id"];
+        $c->ToId = $competition->TourId;
 
-        if (getModuleParameter(PROTOCOL_MODULE, Checked1ParameterName, "0", $_REQUEST["id"]) === "1") {
+        if (getModuleParameter(PROTOCOL_MODULE, Checked1ParameterName, "0", $competition->TourId) === "1") {
             $_REQUEST["country1"] = "1";
             $_REQUEST["StatHeader1"] = getRegionStatisticsHeaderParameter($c, "1");
         }
-        if (getModuleParameter(PROTOCOL_MODULE, Checked2ParameterName, "0", $_REQUEST["id"]) === "1") {
+        if (getModuleParameter(PROTOCOL_MODULE, Checked2ParameterName, "0", $competition->TourId) === "1") {
             $_REQUEST["country2"] = "1";
             $_REQUEST["StatHeader2"] = getRegionStatisticsHeaderParameter($c, "2");
         }
-        if (getModuleParameter(PROTOCOL_MODULE, Checked3ParameterName, "0", $_REQUEST["id"]) === "1") {
+        if (getModuleParameter(PROTOCOL_MODULE, Checked3ParameterName, "0", $competition->TourId) === "1") {
             $_REQUEST["country3"] = "1";
             $_REQUEST["StatHeader3"] = getRegionStatisticsHeaderParameter($c, "3");
         }
