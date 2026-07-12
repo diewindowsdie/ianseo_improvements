86152f30c94cfc64a1604a09687db07edbc03e13
Alexey Malev <diewindowsdie@gmail.com>
2026-07-12 17:44:49 +0300

    Подготовка релиза live_v2.2

--- Common/distro.inc.php
+++ Common/distro.inc.php
@@ -1,7 +1,7 @@
 <?php
 define('ProgramRelease', 'STABLE');
-define('ProgramBuild', 'rev 114 [improvements LIVE v2.1.3]');
-define("CurrentTag", "live_v2.1.3");
+define('ProgramBuild', 'rev 114 [improvements LIVE v2.2]');
+define("CurrentTag", "live_v2.2");
 //pre-релизы собираются из ветки live - это lts ветка, предназначенная для проведения крупных соревнований, в нее попадают только критически важные обновления
 define("UsePreReleases", true);
 define("DefaultCompetitionResultsPagePrefix", "Common/Results/public/competition.php?code=");
